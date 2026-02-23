8e7b6f4c609bdc2e66b9d72323a39f48d6c8d3c6
CyberBrain18 <srihari.nambiar05@gmail.com>
2026-02-23 04:11:29 +0000

    Add new file to MyMavenApp

---
+++ src/main/java/com/example/MainClass.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class MainClass {
+    public static void main(String[] args) {
+        // Initialize two numbers
+        int firstNum = 10;
+        int secondNum = 20;
+
+        // Perform the addition
+        int sum = firstNum + secondNum;
+
+        // Output the result
+        System.out.println("The sum is "  + sum);
+    }
+}
